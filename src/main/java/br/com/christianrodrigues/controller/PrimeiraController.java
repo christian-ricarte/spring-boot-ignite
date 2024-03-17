7e92592cd60201f9a16cfa75ba2d06f87f2e34cc
Christian Rodrigues <crrodrigues2001@outlook.com.br>
2024-03-17 18:50:06 -0300

    some spring boot concepts

---
+++ src/main/java/br/com/christianrodrigues/controller/PrimeiraController.java
@@ -0,0 +1,63 @@
+package br.com.christianrodrigues.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@RestController
+@RequestMapping("/primeiraController")
+public class PrimeiraController {
+    @GetMapping("/primeiroMetodo/{id}")
+    public String primeiroMetodo(@PathVariable String id) {
+        return "O parâmetro que você enviou foi " + id;
+    }
+
+    @GetMapping("/metodoComQueryParams")
+    public String metodoComQueryParams(@RequestParam String id) {
+        return id;
+    }
+
+    @GetMapping("/metodoComQueryParams2")
+    public String metodoComQueryParams2(@RequestParam Map<String, String> allParams) {
+        return "Os parâmetros são " + allParams.entrySet();
+    }
+
+    @PostMapping("/metodoComBodyParams")
+    public String metodoComBodyParams(@RequestBody Usuario usuario) {
+        return usuario.username;
+    }
+
+    @PostMapping("/metodoComHeaders")
+    public String metodoComHeaders(@RequestHeader("name") String name) {
+        return name;
+    }
+
+    @PostMapping("/metodoComListHeaders")
+    public String metodoComListHeaders(@RequestHeader Map<String, String> headers) {
+        return "headers " + headers.entrySet();
+    }
+
+    @GetMapping("/metodoResponseEntity/{id}")
+    public ResponseEntity<Object> metodoResponseEntity(@PathVariable Long id) {
+        var usuario = new Usuario("chrisrodrigues");
+
+        if (id > 5) {
+            return ResponseEntity.status(HttpStatus.OK).body(usuario);
+        } else {
+            return ResponseEntity.badRequest().body("Num. menor que 5");
+        }
+    }
+
+    record Usuario(String username) {
+    }
+}
