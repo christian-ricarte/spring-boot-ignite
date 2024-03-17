1f0bf73da1899f1bb48c8068944dba3fe6f8aba6
Christian Rodrigues <crrodrigues2001@outlook.com.br>
2024-03-17 18:51:43 -0300

    add autowired annotation

--- src/main/java/br/com/christianrodrigues/ioc_di/MeuControllerComponent.java
+++ src/main/java/br/com/christianrodrigues/ioc_di/MeuControllerComponent.java
@@ -2,12 +2,16 @@ package br.com.christianrodrigues.ioc_di;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/component")
 public class MeuControllerComponent {
+    @Autowired
+    MeuComponent meuComponent;
+
     @GetMapping("/")
     public String chamandoComponent() {
         var meuComponent = new MeuComponent();
